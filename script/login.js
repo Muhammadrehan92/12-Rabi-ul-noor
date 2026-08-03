32c3c9274c7b697b4226d08bcb93d338fb1891ae
Apna Name <youremail@example.com>
2026-08-03 15:46:45 +0500

    correct toogle bar menu page

--- script/login.js
+++ script/login.js
@@ -66,3 +66,10 @@ function togglePassword(inputId, button){
     }
 
 }
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
